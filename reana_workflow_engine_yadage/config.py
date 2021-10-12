dd15a3cdb12677cafc5c61339494a04458c1079f
Vladyslav Moisieienkov <vladyslav.moisieienkov@cern.ch>
2021-10-12 09:32:10 +0200

    yadage: remove duplicated messages to MQ
    - removes duplicated status messages to jobs-status MQ
    - sets update interval to 15 seconds
    - refactors REANATracker
    
    Related #204

--- reana_workflow_engine_yadage/config.py
+++ reana_workflow_engine_yadage/config.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 # This file is part of REANA.
-# Copyright (C) 2017, 2018 CERN.
+# Copyright (C) 2017-2021 CERN.
 #
 # REANA is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
@@ -9,8 +9,18 @@
 """REANA Workflow Engine Yadage config."""
 
 import os
+from enum import IntEnum
 
 MOUNT_CVMFS = os.getenv("REANA_MOUNT_CVMFS", "false")
 
 LOGGING_MODULE = "reana-workflow-engine-yadage"
-"""REANA Workflow Engine Yadage logging module."""
+
+WORKFLOW_TRACKING_UPDATE_INTERVAL_SECONDS = 15
+
+
+class WorkflowRunStatus(IntEnum):
+    """Enumeration of **some** possible run statuses of a workflow."""
+
+    running = 1
+    finished = 2
+    failed = 3
